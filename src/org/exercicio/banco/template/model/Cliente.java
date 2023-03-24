dd17bac98dd5f2fc6520fd582e5023adaf393425
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-03-24 16:47:16 -0300

    Atividade concluida

--- src/org/exercicio/banco/template/model/Cliente.java
+++ src/org/exercicio/banco/template/model/Cliente.java
@@ -31,7 +31,12 @@ public class Cliente {
      * @param c
      */
     public void adicionarConta(ContaBancaria c) {
-
+    	if(contas.contains(c)) {
+    		System.out.print("A conta jah estah associada a este cliente.");
+    	}else {
+    		contas.add(c);
+    		System.out.print("Conta adicionada com sucesso!");
+    	}
     }
 
     
@@ -45,7 +50,12 @@ public class Cliente {
      * @param c
      */
     public void removerConta(ContaBancaria c) {
-
+    	if(contas.contains(c)) {
+    		contas.remove(c);
+    		System.out.print("Conta removida com sucesso!");
+    	}else {
+    		System.out.print("A conta nao esta associada a este cliente.");
+    	}
     }
 
     /**
@@ -59,6 +69,12 @@ public class Cliente {
      * @return
      */
     public ContaBancaria localizarContaNumero(int numero) {
+    	for(ContaBancaria conta : contas) {
+    		if(conta.getNumeroConta() == numero) {
+    			System.out.print("Conta encontrada!");
+    			return conta;
+    		}
+    	}System.out.print("Conta nao encontrada.");
         return null;
     }
 
@@ -74,7 +90,13 @@ public class Cliente {
      * @return
      */
     public boolean localizarConta(ContaBancaria c) {
-        return false;
+    	if(contas.contains(c)) {
+    		System.out.print("Conta encontrada!");
+    		return true;
+    	}else {
+    		System.out.print("Conta nao encontrada.");
+    		return false;
+    	}
     }
 
     /**
@@ -86,7 +108,12 @@ public class Cliente {
      * @return
      */
     public double balancoEntreContas() {
-        return 0.0;
+    	double soma = 0.0;
+    	for(ContaBancaria conta : contas) {
+    		soma += conta.getSaldo();    	
+    	}
+    	System.out.print("Balanco entre contas: RS" + soma);
+        return soma;
     }
     
     public ArrayList<ContaBancaria> getContas() {
